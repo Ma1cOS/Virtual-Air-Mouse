0c08d179b6802ca1779ffc43bb453d572106a96a
Ma1cOS <makosotiris@gmail.com>
2026-05-06 18:44:46 +0300

    3 main fingers tracking, baselining the fps for perfomance and optimazation

--- vision_module.py
+++ vision_module.py
@@ -1,5 +1,7 @@
 import cv2
 import mediapipe as mp
+import time
+import math
 
 class HandDetector:
     def __init__(self):
@@ -8,27 +10,51 @@ class HandDetector:
         self.mp_draw = mp.solutions.drawing_utils
         self.hands = self.mp_hands.Hands(
             static_image_mode=False,
+            # 0 for no hand, 1 for one hand, 2 fro both hands
             max_num_hands=1,
+            # gia na trejei me perissotera fps 
+            model_complexity=0,
+            # the system has to be at least 70% sure its a hand before it starts mapping it
             min_detection_confidence=0.7,
             min_tracking_confidence=0.7
         )
 
+
+    # Finding the hand 
     def find_hands(self, img):
+        # Converting colour for the camera (bgr of cv2 to rbg of camera)
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        results = self.hands.process(img_rgb)
+        #trying to find the hand by the img_rgb we converted
+        self.results = self.hands.process(img_rgb)
         
-        if results.multi_hand_landmarks:
-            for hand_lms in results.multi_hand_landmarks:
-                self.mp_draw.draw_landmarks(
-                    img, 
-                    hand_lms, 
-                    self.mp_hands.HAND_CONNECTIONS
-                )
+        if self.results.multi_hand_landmarks:
+            #pass
+            for hand_lms in self.results.multi_hand_landmarks:
+                # if the hand is found, we draw the connections
+                self.mp_draw.draw_landmarks(img, hand_lms, self.mp_hands.HAND_CONNECTIONS)
         return img
+    
+    def find_position(self, img):
+        lm_list = []
+        if self.results.multi_hand_landmarks:
+            my_hand = self.results.multi_hand_landmarks[0]
+            for id, lm in enumerate(my_hand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lm_list.append([id, cx, cy])
+        return lm_list
 
 def main():
     # Δοκίμασε το 0, αν δεν ανοίγει η κάμερα δοκίμασε 1 ή 2
     cap = cv2.VideoCapture(0)
+    # blepw thn analush ths kameras
+    # width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    # height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    # print(f"Ανάλυση Κάμερας: {width}x{height}")
+    
+    # Μεταβλητές για τον υπολογισμό FPS
+    pTime = 0
+    cTime = 0
     
     if not cap.isOpened():
         print("Σφάλμα: Δεν βρέθηκε κάμερα.")
@@ -38,16 +64,36 @@ def main():
 
     while True:
         success, img = cap.read()
-        if not success:
-            print("Αποτυχία λήψης καρέ.")
-            break
+        if not success: break
 
         img = cv2.flip(img, 1)
         img = detector.find_hands(img)
+        lm_list = detector.find_position(img)
 
-        cv2.imshow("Vision Module - Member 1", img)
+        if len(lm_list) != 0:
+            # Σημεία ενδιαφέροντος
+            x4, y4 = lm_list[4][1], lm_list[4][2]   # Αντίχειρας
+            x8, y8 = lm_list[8][1], lm_list[8][2]   # Δείκτης
+            x12, y12 = lm_list[12][1], lm_list[12][2] # Μεσαίος
+
+            # Σχεδίαση κύκλων στα 3 βασικά δάκτυλα
+            cv2.circle(img, (x4, y4), 10, (0, 255, 255), cv2.FILLED) # Κίτρινο
+            cv2.circle(img, (x8, y8), 10, (0, 255, 0), cv2.FILLED)   # Πράσινο
+            cv2.circle(img, (x12, y12), 10, (255, 0, 0), cv2.FILLED) # Μπλε
+
+            # Παράδειγμα υπολογισμού απόστασης (για το Μέλος 3)
+            distance = math.hypot(x8 - x4, y8 - y4)
+            if distance < 30: # Αν "τσιμπήσεις" τον δείκτη
+                cv2.circle(img, (x8, y8), 15, (0, 0, 255), cv2.FILLED) # Γίνεται κόκκινο
+
+        # Υπολογισμός και εμφάνιση FPS
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, f'FPS: {int(fps)}', (20, 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
+
+        cv2.imshow("Vision Test - 3 Fingers", img)
         
-        # Κλείνει με το πλήκτρο 'q'
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
